42e0df11dbc2d176d50aa8b0ce81e0eb1ef2c55b
Aamir@786 <aamirhussainazad786@gmail.com>
2024-06-29 00:02:19 +0530

    commit add

---
+++ Q643MaxAverageSubarray.java
@@ -0,0 +1,32 @@
+public class Q643MaxAverageSubarray {
+
+    public double findMaxAverage(int[] nums, int k) {
+        double res = -1111;
+        int i = 0;
+        int j = 0;
+        double sum = 0;
+        while (j <= nums.length - 1) {
+            sum += nums[j];
+            if (j - i + 1 == k) {
+                res = Math.max(res, (sum / k));
+                sum -= nums[i++];
+            }
+            j++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        Q643MaxAverageSubarray obj = new Q643MaxAverageSubarray();
+
+        int[] nums = {1, 12, -5, -6, 50, 3};
+
+        int k = 4;
+
+        double result = obj.findMaxAverage(nums, k);
+
+        System.out.println(result);
+
+    }
+}
