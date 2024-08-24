96cd09a4246b9efa460d035922519dceefd611cc
Aamir@786 <aamirhussainazad786@gmail.com>
2024-08-24 19:38:20 +0530

    commit add

--- BST.java
+++ BST.java
@@ -1,3 +1,5 @@
+// package tr;
+// import TreeNode;
 
 class TreeNode {
 
--- Leetcode150Qusetions/Q88MergeSortedArrays.java
+++ Leetcode150Qusetions/Q88MergeSortedArrays.java
@@ -1,31 +1,73 @@
+// package Leetcode150Qusetions;
+// import java.util.Arrays;
+// public class Q88MergeSortedArrays {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int i = m-1;
+//         int j = n-1;
+//         int k = m+n-1;
+//         while(i>=0 && j>=0){
+//             if(nums1[i] > nums2[j]){
+//                 nums1[k--] = nums1[i--];
+//             }else{
+//                 nums1[k--] = nums2[j--];
+//             }
+//         }
+//         while(j>=0){
+//             nums1[k--] = nums2[j--];
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Q88MergeSortedArrays obj = new Q88MergeSortedArrays();
+//         int[] nums1 = {1,2,3,0,0,0};
+//         int m = 3;
+//         int[] nums2 = {2,5,6};
+//         int n = 3;
+//         obj.merge(nums1, m, nums2, n);
+//         System.out.println(Arrays.toString(nums1));
+//     }
+// }
 
-import java.util.Arrays;
+
+
+
+
+
+
+
+
+package Leetcode150Qusetions;
 
 public class Q88MergeSortedArrays {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
-            }else{
-                nums1[k--] = nums2[j--];
-            }
+
+    // Function to find the GCD of two numbers
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
-        while(j>=0){
-            nums1[k--] = nums2[j--];
-        }
+        return gcd(b, a % b);
     }
 
-    public static void main(String[] args) {
-        Q88MergeSortedArrays obj = new Q88MergeSortedArrays();
-        int[] nums1 = {1,2,3,0,0,0};
-        int m = 3;
-        int[] nums2 = {2,5,6};
-        int n = 3;
-        obj.merge(nums1, m, nums2, n);
-        System.out.println(Arrays.toString(nums1));
+    // Function to find the GCD of an array
+    public static int findGCD(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array must contain at least one element.");
+        }
+        
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = gcd(result, arr[i]);
+
+            // If at any point GCD becomes 1, return 1
+            if (result == 1) {
+                return 1;
+            }
+        }
+        return result;
     }
-    
+
+    // Main method for testing
+    public static void main(String[] args) {
+        int[] arr = {24, 36, 48, 60};  // Example array
+        System.out.println("GCD of the array: " + findGCD(arr));  // Expected Output: 12
+    }
 }
